632ed865b348c5897419d187ce094513e19abaaa
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-06-05 20:47:55 +0300

    Add files via upload

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton.GetInstance().WriteLog("hello");
+            //Singleton.GetInstance().WriteLog("world");
+
+            string k = "k";    
+            
+            Singleton.GetInstance().WriteLog(k);
+        }
+
+
+        
+    }
+}
---
+++ ConsoleApp31/Singleton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        string path = "C:\\Users\\HackerU\\Desktop\\check\\check.txt";
+        string text;
+
+        private static object key = new object();
+        private Singleton()
+        {
+
+        }
+
+        public void WriteLog(string log)
+        {
+            text = text + log;
+            File.WriteAllText(path, text);
+            
+       
+        }
+
+        public static Singleton GetInstance()
+        {  
+            if (_instance == null)
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
